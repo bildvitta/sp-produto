028c9ce7e1f3af82364d5144c7478fd8231fb730
Paulo Davanço <paulodavanco@gmail.com>
2022-08-04 11:31:26 -0300

    Fix migrations

--- src/SpProdutoServiceProvider.php
+++ src/SpProdutoServiceProvider.php
@@ -29,24 +29,24 @@ class SpProdutoServiceProvider extends PackageServiceProvider
             ->name('sp-produto')
             ->hasConfigFile(['sp-produto'])
             ->hasMigrations([
+                'create_sp_produto_real_estate_developments_table', // must be the first
+                'create_sp_produto_accessory_categories_table', // must be before the accessories table
                 'create_sp_produto_accessories_table',
-                'create_sp_produto_accessory_categories_table',
+                'create_sp_produto_blueprints', // must be before others blueprints tables
                 'create_sp_produto_blueprint_images_table',
                 'create_sp_produto_blueprint_real_estate_development_accessory_table',
                 'create_sp_produto_blueprint_typology_table',
-                'create_sp_produto_blueprints',
+                'create_sp_produto_buying_options_table', // must be before others buying options tables
                 'create_sp_produto_buying_option_real_estate_development_table',
-                'create_sp_produto_buying_options_table',
                 'create_sp_produto_characteristics_table',
+                'create_sp_produto_proposal_models_table', // must be before others proposal models tables
                 'create_sp_produto_proposal_model_periodicities_table',
                 'create_sp_produto_proposal_model_real_estate_development_table',
                 'create_sp_produto_proposal_model_typology_table',
-                'create_sp_produto_proposal_models_table',
                 'create_sp_produto_real_estate_development_accessories_table',
                 'create_sp_produto_real_estate_development_characteristic_table',
-                'create_sp_produto_real_estate_developments_table',
                 'create_sp_produto_stages_table',
-                'create_sp_produto_typologies_table',
+                'create_sp_produto_typologies_table', // must be before others typologies tables
                 'create_sp_produto_typology_attributes_table',
                 'create_sp_produto_units_table',
             ])
